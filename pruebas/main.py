8df1a3fa0286b21eeaa3b2fb2dfcb0a006ec7df9
Medel Piña Alberto Natanael <natanael_pi@ciencias.unam.mx>
2022-03-02 13:15:04 -0600

    Llamada exitosa

--- pruebas/main.py
+++ pruebas/main.py
@@ -1,5 +1,9 @@
 import requests as req
 
-datos = req.get("http://api.openweathermap.org/data/2.5/weather?lat={19.3371}&lon={-99.566}&appid={-API_KEY-}")
+"""
+Una llmada simple a la API con la coordenadas de TLC.
+NOTA: retirar los corchetes de los parámteros xd.
+"""
+datos = req.get("http://api.openweathermap.org/data/2.5/weather?lat=19.3371&lon=-99.566&appid=7dde62e26895fcefea7a7aceb8e4c96d")
 
 print(datos.json())
