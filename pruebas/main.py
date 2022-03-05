69e5b689080e019416d7ee8e5a108239b79502d3
shucshin <robinucscs@gmail.com>
2022-03-04 18:59:19 -0600

    Update

--- pruebas/main.py
+++ pruebas/main.py
@@ -1,5 +1,5 @@
 import requests as req
 
-datos = req.get("http://api.openweathermap.org/data/2.5/weather?lat={19.3371}&lon={-99.566}&appid={-API_KEY-}")
+datos = req.get("http://api.openweathermap.org/data/2.5/weather?lat={19.3371}&lon={-99.566}&appid={a}")
 
 print(datos.json())
