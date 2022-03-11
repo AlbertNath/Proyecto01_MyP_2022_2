062d5b1ac84fbac4cf2dd6373131909269636b0b
Medel Piña Alberto Natanael <natanael_pi@ciencias.unam.mx>
2022-03-10 21:13:05 -0600

    Clase aereopuerto

---
+++ src/main/aereopuerto.py
@@ -0,0 +1,25 @@
+class aereopuerto:
+    """
+    Clase para represenar un aereopuerto.
+    """
+    def __init__(self, respDepurada:dict, codigo:str):
+        """Constructor de clase"""
+        self.codigo_IATA = codigo
+        self.clima = respDepurada['clima']
+        self.descripcion = respDepurada['descripcion']
+        self.temperatura = respDepurada['temperatura']
+        self.sensacion_term = respDepurada['sensacion_term']
+        self.viento_vel = respDepurada['sensacion_term']
+        self.nubosidad = respDepurada['nubosidad']
+
+
+    def toString(self):
+        str = "Aereopuerto: {}\nClima: {}\nDescripción: {}\nTemperatura: {}\nSensación térmica: {}\n Velocidad viento:{}\nNubosidad: {}\n"
+
+        return str.format(self.codigo_IATA,
+                          self.clima,
+                          self.descripcion,
+                          self.temperatura,
+                          self.sensacion_term,
+                          self.viento_vel,
+                          self.nubosidad)
